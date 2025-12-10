75ce3023b8b67d76461b989391db48f8f8b2ed4b
Govind Siwakoti <mesmer@mail.com>
2025-12-10 13:42:08 +0545

    fix: fixed tsc

--- src/server/root.ts
+++ src/server/root.ts
@@ -1,10 +1,8 @@
 import { router } from "@/server/trpc";
 import { userRouter } from "@/routers/user";
-import { uploadRouter } from "@/routers/upload";
 import { adGenerationRouter } from "@/routers/adGeneration";
 
 export const appRouter = router({
-  upload: uploadRouter,
   user: userRouter,
   adGeneration: adGenerationRouter,
 });
